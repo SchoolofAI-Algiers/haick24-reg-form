dbd26f317005ad88fd839e8ec13057782c7fcdf2
lahcenbcf <h.bencharef@esi-sba.dz>
2024-06-20 19:09:29 +0100

    save

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,8 +10,8 @@ const Modal = ({ show, handleClose, message, codeTeam ,isError}) => {
         <button className="close-button" onClick={handleClose}>X</button>
         <h4 className={`font-bold text-primary ${isError && "text-red-600"}`}>{ !isError ? message : "Submission failed"}</h4>
         {!isError ?
-       ( (codeTeam &&codeTeam!="#")  ? <h2>Your team code is: <strong>{codeTeam + " "} <span  className="underline block font-thin text-red-500">share with your team members for their registrations!</span></strong></h2> :<h2>thanks, we will contact you soon !</h2> )
-: <h2>we are sorry, please repeat !</h2>
+       ( (codeTeam &&codeTeam!="#")  ? <h3>Your team code is: <strong>{codeTeam + " "} <span  className="underline block font-thin text-red-500">share with your team members for their registrations!</span></strong></h3> :<h3>thanks, we will contact you soon !</h3> )
+: <h3>we are sorry, please repeat !</h3>
 }
       </div>
     </div>
