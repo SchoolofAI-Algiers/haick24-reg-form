73a8d4c7c804dd34252e74b6d0a2fb48b222495b
Hassane BENCHAREF <107793995+lahcenbcf@users.noreply.github.com>
2024-06-24 21:00:23 +0100

    registration closed

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,26 @@ function App() {
       <img src={Grid} className="w-full object-cover max-h-[calc(100vh)] absolute bottom-0 z-0" />
       <img src={curve1} className="h-screen absolute -left-20  pt-[120px]" />
       <img src={curve2} className="absolute right-0 top-32 xl:top-16 pt-[120px]" />
-      <img src={curve3} className="absolute bottom-0 translate-x-[20%] translate-y-[0%] z-10" />
+      <img src={curve3} className="absolute bottom-0 translate-x-[20%] translate-y-[0%] -z-10" />
       <img src={curve4} className="absolute left-0 -bottom-0 z-10" />
       <div className='h-56 w-56 bg-primary absolute top-20 blur-[200px] rounded-full -z-40' />
       <div className='h-56 w-56 bg-primary absolute bottom-20 right-9 blur-[100px] rounded-full -z-40' />
       <img src={logoinverse} className="absolute left-[80px] bottom-[10px]" />
 
       <div>
+        {/*
         <Header />
         <FormPage />
+         */}
+
+         <div className='flex flex-col items-center max-w-xl mx-auto shadow-lg rounded-md absolute top-[50%] left-[50%] -translate-y-[50%] -translate-x-[50%]'>
+              <h1 className='font-bold text-white text-[60px] text-center'>Registration are <br/>
+              <span className=' text-primary'>Closed</span></h1>
+              <div className='px-4 py-2 rounded-lg bg-slate-500 font-semibold text-white z-50 w-fit'>
+                <h3>{"registration.setState("}<span className='text-red-700'>{"'closed');"}</span></h3>
+              </div>
+         </div>
+        
       </div>
     </div>
   );
